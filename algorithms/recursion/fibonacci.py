f09bda19c8261cd71acd52c41d932e664cfc31de
nabeelahmedjh <abdulnabeel40@gmail.com>
2022-09-25 15:50:39 +0500

    updated the layout of the repo

---
+++ algorithms/recursion/fibonacci.py
@@ -0,0 +1,21 @@
+"""Implement a function recursively to get the desired
+Fibonacci sequence value.
+Your code should have the same input/output as the 
+iterative code in the instructions."""
+
+
+def get_fib(position):
+    if position < 0:
+        return "Not valid"
+    elif position == 1:
+        return 1
+    elif position == 0:
+        return 0
+    else:
+        return get_fib(position - 1) + get_fib(position - 2)
+
+
+# Test cases
+print get_fib(9)
+print get_fib(11)
+print get_fib(0)
