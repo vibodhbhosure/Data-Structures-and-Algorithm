6cdbbf03c861d8aec082d80d3217e88510112c8b
Azan <azanrashid26@gmail.com>
2022-10-01 21:12:02 +0500

    Leetcode 141. Linked List Cycle added
    CPP solution for Linked List Cycle problem.

---
+++ leetcode-solutions/Linked_List_Cycle.cpp
@@ -0,0 +1,19 @@
+// This approach uses Floyd's Cycle Detection Algorithm
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(slow!=NULL && fast != NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow==fast){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
